3552c31032e6015f37fb301bd36d98aedbb40258
Vishal Srivastava <vishal.sri0807@gmail.com>
2019-10-09 10:30:25 +0530

    Update models.py

--- customer/models.py
+++ customer/models.py
@@ -12,6 +12,7 @@ class Customer(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     phone_number = models.CharField(validators=[phone_regex], max_length=10, blank=True) # validators should be a list
     address=models.CharField(max_length=40)
+    gender = models.CharField(max_length=1)
 
     def __unicode__(self):
         return "%s" % (self.name)
